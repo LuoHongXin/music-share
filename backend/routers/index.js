4bcc4e491145899134c1358b38860b345394c39e
kamiaiqiaodaima <954108736@qq.com>
2020-06-20 11:30:37 +0800

    一部分后台代码

---
+++ backend/routers/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+router.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Content-Type,Content-Length,Authorization,Accept,x-request-width");
+    res.header("Access-Control-Allow-Methods","PUT,PATCH,POST,GET,DELETE,OPTIONS");
+    if(req.method=="OPTIONS"){
+        res.sendStatus(200);
+    }else{
+        next();
+    }
+})
+//引入路由
+const upload = require('./upload');
+
+
+router.use(express.urlencoded({extended:true}),express.json());
+
+
+router.use('/upload',upload);
+
+module.exports = router;
