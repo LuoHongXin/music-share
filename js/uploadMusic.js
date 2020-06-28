0207d74ec0a45df5b473c38c54c76c280f5e05f4
kamiaiqiaodaima <954108736@qq.com>
2020-06-28 14:03:36 +0800

    ajaxfrom

--- js/uploadMusic.js
+++ js/uploadMusic.js
@@ -9,10 +9,28 @@ uploadMusic.prototype.init = function () {
 uploadMusic.prototype.event = function () {
     $('.upload-music #uploadMusic').change(function(){
         var file = $(this)[0].files[0];
+        console.log(file);
         if (!/audio\/\w+/.test(file.type)) {/*可以把autio改成其他文件类型 比如 image*/
              $(this).val('')  
             alert("只能选择音频文件");
         }
+        $(".upload-music .msg").removeClass("hide red").html(file.name);
+    })
+    $(".upload-music .uploadfile").click(function(){
+        $("#uploadMusic").click();
+    })
+    $(".submit").click(function(){
+        if(!$("#uploadMusic").val()){
+            $(".upload-music .msg").removeClass("hide").addClass("red").html("请先上传文件")
+            return false;
+        }else{
+            $(".upload-music .msg").removeClass("red").addClass("hide").html("")
+        }
+    })
+    $(".upload-music form").ajaxForm({
+        success:function(data){
+            console.log(data);
+        }
     })
 }
 
